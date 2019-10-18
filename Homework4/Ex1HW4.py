22d894c3d1c73e4d7a5a8c8abdc3f7544fd4b74c
sr68731 <55667645+sr68731@users.noreply.github.com>
2019-10-18 17:44:45 +0200

    Add files via upload

--- Homework4/Ex1HW4.py
+++ Homework4/Ex1HW4.py
@@ -15,14 +15,19 @@ beta=0.988 #standard US rate of impatience
 theta=0.679 #labour share
 delta=0.013 #standard US depreciation rate
 h=1 #fixed labour supply
-kappa=5.24
+kappa=0
+#kappa=5.24
 nu=2
 
 k_low=5 #set lower bound for the capital grid
 k_high=55 #set upper bound for the capital grid
-fine=1001 #set density of the grid
+fine=101 #set density of the grid
 
 k_grid=np.linspace(k_low, k_high, fine)
+
+vfpx=np.zeros((1,fine-1)) #initialising x-grid for plotting valfun
+for i in range(0, fine-1):
+    vfpx[0,i]=k_grid[i]
 
 ##############################################################################
 #VFI
@@ -97,17 +102,12 @@ k_ss=k_path[0, iter-1]
 #Valfun plotting
 ##############################################################################
 
-vfpx=np.zeros((1,fine-1)) #initialising x-grid for plotting valfun
-for i in range(0, fine-1):
-    vfpx[0,i]=k_grid[i]
-
-
 mpl.pyplot.scatter(vfpx, valfun, s=1, color='blue')
 mpl.pyplot.xlabel('capital')
 mpl.pyplot.ylabel('value function')
 mpl.pyplot.legend()
 #mpl.pyplot.xlim([0, 200])
-mpl.pyplot.ylim([0, -500])
+#mpl.pyplot.ylim([0, -500])
 mpl.pyplot.title(r"Value function")
 mpl.pyplot.show()
 
@@ -152,13 +152,8 @@ iter=iter+1
 while iter<maxiter and progress>conv:
     valfun_old=np.copy(valfun)
     for i in range (0, fine-1):
-        if i==0: #for the k_low nothing changes, monotonicity can't help here
-            chi[i,]=M[i,]+beta*valfun
-        else: #for higher capital levels ve can limit number of chi updates
-            for j in range(int(g[0,i-1]), fine-1):
-                chi[i,j]=M[i,j]+beta*valfun[0,j]
-            #for j in range(0, int(g[0,i-1])-1): #let's see XD, but it seems that it works
-                #chi[i,j]=-1000000
+        for j in range(int(g[0,i]), fine-1):
+            chi[i,j]=M[i,j]+beta*valfun[0,j]
     for i in range(0, fine-1):
         valfun[0,i]=np.amax(chi[i,]) #update valfun
         g[0,i]=np.argmax(chi[i,])
@@ -173,7 +168,7 @@ mpl.pyplot.xlabel('capital')
 mpl.pyplot.ylabel('value function')
 mpl.pyplot.legend()
 #mpl.pyplot.xlim([0, 200])
-mpl.pyplot.ylim([0, -500])
+#mpl.pyplot.ylim([0, -500])
 mpl.pyplot.title(r"Value function under g-monotonicity")
 mpl.pyplot.show()
 
@@ -207,6 +202,16 @@ conv=0.001 #convergence level
 chi=np.zeros((fine-1, fine-1)) #initialize chi
 progress=1
 
+#first iteration goes as it used to go, because we need initial g
+valfun_old=np.copy(valfun) #store old valfun
+for i in range(0, fine-1):
+    chi[i,]=M[i,]+beta*valfun #update chi
+for i in range(0, fine-1):
+    valfun[0,i]=np.amax(chi[i,]) #update valfun
+    g[0,i]=np.argmax(chi[i,])
+progress=abs(np.amax(valfun-valfun_old)) #check progress
+iter=iter+1
+
 while iter<maxiter and progress>conv:
 #for j in range(0, maxiter):
     valfun_old=np.copy(valfun)
@@ -216,9 +221,242 @@ while iter<maxiter and progress>conv:
             if j>0 and chi[i,j]<chi[i,j-1]:
                 #g[0,i]=j
                 break
+            valfun[0,i]=np.amax(chi[i,]) #update valfun
+            g[0,i]=np.argmax(chi[i,])
+    progress=abs(np.amax(valfun-valfun_old))
+    iter=iter+1
+
+toc=time.time()
+
+mpl.pyplot.scatter(vfpx, valfun, s=1, color='blue')
+mpl.pyplot.xlabel('capital')
+mpl.pyplot.ylabel('value function')
+mpl.pyplot.legend()
+#mpl.pyplot.xlim([0, 200])
+#mpl.pyplot.ylim([0, -500])
+mpl.pyplot.title(r"Value function under valfun-concavity")
+mpl.pyplot.show()
+
+print("Value function iteration required ", iter, " iterations")
+print("Value function iterations took ", toc-tic, " seconds")
+
+##############################################################################
+#Decision rule monotonicity and value function concavity
+##############################################################################
+
+tic=time.time()
+
+valfun=np.zeros((1,fine-1)) #create array of zeros to store value function
+g=np.zeros((1, fine-1)) #create array to store indices of optimal capital levels (useful for policy function)
+
+#Now we'll build the return matrix:
+
+M=np.zeros((fine-1, fine-1)) #initialize the matrix
+for i in range(0, fine-1):
+    for j in range(0, fine-1):
+        if (k_grid[i]**(1-theta))*((h)**theta)-k_grid[j]+(1-delta)*k_grid[i]>=0:
+            M[i,j]=np.log((k_grid[i]**(1-theta))*((h)**theta)-k_grid[j]+(1-delta)*k_grid[i])-kappa*(h**(1+1/nu))/(1+1/nu)
+        else: M[i,j]=-1000000
+        
+#Now true iteration starts
+        
+iter=0 #number of completed iterations
+maxiter=1000 #maximum number of iterations
+conv=0.001 #convergence level
+
+chi=np.zeros((fine-1, fine-1)) #initialize chi
+progress=1
+
+#first iteration goes as it used to go, because we need initial g
+valfun_old=np.copy(valfun) #store old valfun
+for i in range(0, fine-1):
+    chi[i,]=M[i,]+beta*valfun #update chi
+for i in range(0, fine-1):
     valfun[0,i]=np.amax(chi[i,]) #update valfun
     g[0,i]=np.argmax(chi[i,])
+progress=abs(np.amax(valfun-valfun_old)) #check progress
+iter=iter+1
+
+while iter<maxiter and progress>conv:
+#for j in range(0, maxiter):
+    valfun_old=np.copy(valfun)
+    for i in range(0, fine-1):
+        for j in range(int(g[0,i]), fine-1):
+            chi[i,j]=M[i,j]+beta*valfun[0,j]
+            if j>0 and chi[i,j]<chi[i,j-1]:
+                #g[0,i]=j
+                break
+            valfun[0,i]=np.amax(chi[i,]) #update valfun
+            g[0,i]=np.argmax(chi[i,])
     progress=abs(np.amax(valfun-valfun_old))
+    iter=iter+1
+
+toc=time.time()
+
+mpl.pyplot.scatter(vfpx, valfun, s=1, color='blue')
+mpl.pyplot.xlabel('capital')
+mpl.pyplot.ylabel('value function')
+mpl.pyplot.legend()
+#mpl.pyplot.xlim([0, 200])
+#mpl.pyplot.ylim([0, -500])
+mpl.pyplot.title(r"Value function under g-monotonicity and valfun-concavity")
+mpl.pyplot.show()
+
+print("Value function iteration required ", iter, " iterations")
+print("Value function iterations took ", toc-tic, " seconds")
+
+##############################################################################
+#Local search
+##############################################################################
+
+tic=time.time()
+
+local=5 #define bounds for the local search
+
+valfun=np.zeros((1,fine-1)) #create array of zeros to store value function
+g=np.zeros((1, fine-1)) #create array to store indices of optimal capital levels (useful for policy function)
+
+#Now we'll build the return matrix:
+
+M=np.zeros((fine-1, fine-1)) #initialize the matrix
+for i in range(0, fine-1):
+    for j in range(0, fine-1):
+        if (k_grid[i]**(1-theta))*((h)**theta)-k_grid[j]+(1-delta)*k_grid[i]>=0:
+            M[i,j]=np.log((k_grid[i]**(1-theta))*((h)**theta)-k_grid[j]+(1-delta)*k_grid[i])-kappa*(h**(1+1/nu))/(1+1/nu)
+        else: M[i,j]=-1000000
+        
+#Now true iteration starts
+        
+iter=0 #number of completed iterations
+maxiter=1000 #maximum number of iterations
+conv=0.001 #convergence level
+
+chi=np.zeros((fine-1, fine-1)) #initialize chi
+progress=1
+
+while iter<maxiter and progress>conv:
+    valfun_old=np.copy(valfun) #store old valfun
+    for i in range(0, local-1):
+        chi[i,]=M[i,]+beta*valfun #update chi
+    for i in range(local, fine-1-local):
+        for j in range(i-local, i+local):
+            chi[i,j]=M[i,j]+beta*valfun[0,j]
+    for i in range(fine-local, fine-1):
+        chi[i,]=M[i,]+beta*valfun #update chi
+    for i in range(0, fine-1):
+        valfun[0,i]=np.amax(chi[i,]) #update valfun
+        g[0,i]=np.argmax(chi[i,])
+    progress=abs(np.amax(valfun-valfun_old)) #check progress
+    iter=iter+1
+
+toc=time.time()
+
+mpl.pyplot.scatter(vfpx, valfun, s=1, color='blue')
+mpl.pyplot.xlabel('capital')
+mpl.pyplot.ylabel('value function')
+mpl.pyplot.legend()
+#mpl.pyplot.xlim([0, 200])
+#mpl.pyplot.ylim([0, -500])
+mpl.pyplot.title(r"Value function under local search")
+mpl.pyplot.show()
+
+print("Value function iteration required ", iter, " iterations")
+print("Value function iterations took ", toc-tic, " seconds")
+
+##############################################################################
+#Howard method
+##############################################################################
+
+tic=time.time()
+
+valfun=np.zeros((1,fine-1)) #create array of zeros to store value function
+g=np.zeros((1, fine-1)) #create array to store indices of optimal capital levels (useful for policy function)
+
+#Now we'll build the return matrix:
+
+M=np.zeros((fine-1, fine-1)) #initialize the matrix
+for i in range(0, fine-1):
+    for j in range(0, fine-1):
+        if (k_grid[i]**(1-theta))*(h**theta)-k_grid[j]+(1-delta)*k_grid[i]>=0:
+            M[i,j]=np.log((k_grid[i]**(1-theta))*((h)**theta)-k_grid[j]+(1-delta)*k_grid[i])-kappa*(h**(1+(1/nu)))/(1+(1/nu))
+        else: M[i,j]=-1000000
+        
+#Now true iteration starts
+        
+iter=0 #number of completed iterations
+maxiter=1000 #maximum number of iterations
+conv=0.001 #convergence level
+
+chi=np.zeros((fine-1, fine-1)) #initialize chi
+progress=1
+
+while iter<maxiter and progress>conv:
+    valfun_old=np.copy(valfun) #store old valfun
+    if iter<200:
+        for i in range(0, fine-1):
+            chi[i,]=M[i,]+beta*valfun #update chi
+        for i in range(0, fine-1):
+            valfun[0,i]=np.amax(chi[i,]) #update valfun
+        progress=abs(np.amax(valfun-valfun_old))
+        iter=iter+1
+    else:
+        for i in range(0, fine-1):
+            chi[i,]=M[i,]+beta*valfun #update chi
+        for i in range(0, fine-1):
+            valfun[0,i]=np.amax(chi[i,]) #update valfun
+            g[0,i]=np.argmax(chi[i,])
+        progress=abs(np.amax(valfun-valfun_old))
+        iter=iter+1
+
+toc=time.time()
+
+print("Value function iteration required ", iter, " iterations")
+print("Value function iterations took ", toc-tic, " seconds")
+
+mpl.pyplot.scatter(vfpx, valfun, s=1, color='blue')
+mpl.pyplot.xlabel('capital')
+mpl.pyplot.ylabel('value function')
+mpl.pyplot.legend()
+mpl.pyplot.title(r"Value function with Howard method, policy iterations from iter=200")
+mpl.pyplot.show()
+
+##############################################################################
+#Howard method with regular policy improvements
+##############################################################################
+
+tic=time.time()
+
+valfun=np.zeros((1,fine-1)) #create array of zeros to store value function
+g=np.zeros((1, fine-1)) #create array to store indices of optimal capital levels (useful for policy function)
+
+#Now we'll build the return matrix:
+
+M=np.zeros((fine-1, fine-1)) #initialize the matrix
+for i in range(0, fine-1):
+    for j in range(0, fine-1):
+        if (k_grid[i]**(1-theta))*(h**theta)-k_grid[j]+(1-delta)*k_grid[i]>=0:
+            M[i,j]=np.log((k_grid[i]**(1-theta))*((h)**theta)-k_grid[j]+(1-delta)*k_grid[i])-kappa*(h**(1+(1/nu)))/(1+(1/nu))
+        else: M[i,j]=-1000000
+        
+#Now true iteration starts
+        
+iter=0 #number of completed iterations
+maxiter=1000 #maximum number of iterations
+conv=0.001 #convergence level
+
+chi=np.zeros((fine-1, fine-1)) #initialize chi
+progress=1
+
+while iter<maxiter and progress>conv:
+#for j in range(0, maxiter):
+    valfun_old=np.copy(valfun) #store old valfun
+    for i in range(0, fine-1):
+        chi[i,]=M[i,]+beta*valfun #update chi
+    for i in range(0, fine-1):
+        valfun[0,i]=np.amax(chi[i,]) #update valfun
+        if iter%50==0:
+            g[0,i]=np.argmax(chi[i,])
+    progress=abs(np.amax(valfun-valfun_old)) #check progress
     iter=iter+1
 
 toc=time.time()
@@ -226,10 +464,12 @@ toc=time.time()
 print("Value function iteration required ", iter, " iterations")
 print("Value function iterations took ", toc-tic, " seconds")
 
-
-
-
-
+mpl.pyplot.scatter(vfpx, valfun, s=1, color='blue')
+mpl.pyplot.xlabel('capital')
+mpl.pyplot.ylabel('value function')
+mpl.pyplot.legend()
+mpl.pyplot.title(r"Value function with Howard method, one policy iteration per 50 iterations")
+mpl.pyplot.show()
 
 
 
